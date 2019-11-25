26ec48755a8a552ef9d793d5e178f4563dd7323a
Mikaela Peters <petermik@oregonstate.edu>
2019-11-25 15:56:34 -0800

    16 fixed

--- main.js
+++ main.js
@@ -51,10 +51,10 @@
 $(updateView)
 
 //Arrays to hold data points pulled from JQUERY
-1et years = []
-1et months2016 = []
-1et months2017 = []
-1et months2018 = []
+let years = []
+let months2016 = []
+let months2017 = []
+let months2018 = []
 
 //define or implement function
 function updateView(){
@@ -65,17 +65,17 @@ function updateView(){
 }
 
 function perYear(data){
-  for(var index= 0, month=9, index <= 3, month <= 12; ++index, ++month){
+  for(var index= 0, month=9; index <= 3, month <= 12; ++index, ++month){
     months2016.push(data[2016] [index] [month])
   }
   console.log("2016 data by months is easy "+ months2016)
 
-  for(var index= 0, month=1, index <= 11, month <= 12; ++index, ++month){
+  for(var index= 0, month=1; index <= 11, month <= 12; ++index, ++month){
     months2017.push(data[2017] [index] [month])
   }
   console.log("2017 data by months is easy "+ months2017)
 
-  for(var index= 0, month=1, index <= data.length, month <= 10; ++index, ++month){
+  for(var index= 0, month=1; index <= data.length, month <= 10; ++index, ++month){
     months2018.push(data[2018] [index] [month])
   }
   console.log("2018 data by months is easy "+ months2018)
