02236b5c685dcdb211621c5afa28f01a5d0a82e7
Mikaela Peters <petermik@oregonstate.edu>
2019-11-25 15:01:54 -0800

    Fix

---
+++ main.js
@@ -0,0 +1,44 @@
+// alert("Hey you!")
+
+// $(updateview)
+
+// function updateview() {
+//     alert("Take a moment to smile! :)")
+// }
+
+const BASE_URL = "https://zagster-service.herokuapp.com"
+
+$(updateGraph)
+
+function updateView() {
+  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+}
+
+function updateRideCount(data) {
+  numberOfRides = data.count
+  $("h2#rideCount").html(numberOfRides)
+  console.log(numberOfRides)
+}
+
+
+function updateGraph() {
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+}
